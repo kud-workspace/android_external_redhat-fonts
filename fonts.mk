87bfdbfd23488ba2a5e6ea7f20ddeebdf3d1bda1
Albert I <kras@raphielgang.org>
2021-07-11 20:40:13 +0800

    RedHatFont: Include into AOSP as Roboto drop-in replacement
    Include Red Hat Display and Red Hat Text as sans-serif and sans-serif-condensed
    Roboto replacement, respectively. Font names aren't patched in any way, but has
    files "installed" as renamed for such purpose.
    
    Provide symlink of Roboto Regular to Red Hat Display Regular since recovery
    always expects Roboto Regular for its image generator.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

---
+++ fonts.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2015 The Android Open Source Project
+# Copyright (C) 2021 KudProject Development
+# SPDX-License-Identifier: Apache-2.0
+
+PRODUCT_PACKAGES := \
+    Roboto-Regular.ttf \
+    Roboto-Bold.ttf \
+    Roboto-Italic.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Black.ttf \
+    Roboto-BlackItalic.ttf \
+    Roboto-Light.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Medium.ttf \
+    Roboto-MediumItalic.ttf \
+    RobotoCondensed-Regular.ttf \
+    RobotoCondensed-Bold.ttf \
+    RobotoCondensed-Italic.ttf \
+    RobotoCondensed-BoldItalic.ttf \
+    RobotoCondensed-Light.ttf \
+    RobotoCondensed-LightItalic.ttf \
+    RobotoCondensed-Medium.ttf \
+    RobotoCondensed-MediumItalic.ttf
